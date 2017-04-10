dcc1765c13adb742021459273d85902f816e9a9c
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2017-04-10 17:23:23 +0100

    :construction: Remove publish_date column from table and add title column

---
+++ db/migrate/20170410162115_add_titles_to_posts.rb
@@ -0,0 +1,5 @@
+class AddTitlesToPosts < ActiveRecord::Migration[5.0]
+  def change
+    add_column :posts, :title, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170410135330) do
+ActiveRecord::Schema.define(version: 20170410162115) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "article"
     t.string   "author"
-    t.datetime "publish_date"
     t.string   "category"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "title"
   end
 
 end
