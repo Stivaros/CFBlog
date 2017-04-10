e14eac499b1aef780e6a9b981ddaf26da1885efb
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2017-04-11 00:19:18 +0100

    :sparkles::lipstick::bento: Generate aesthetically pleasing Views
    Views are now styled, with some bootstrap styles and some custom styles for a balance between ease of creation and final aesthetic
    
    Blogs on home page also show in descending creation order (the newest post is at the top)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,39 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function() {
+  
+  // navigation will scroll then stick to top of page
+  $(window).scroll(function () {
+      //if you hard code, then use console
+      //.log to determine when you want the 
+      //nav bar to stick.  
+      console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 180) {
+      $('.nav-menu').addClass('navbar-fixed');
+    }
+    if ($(window).scrollTop() < 180) {
+      $('.nav-menu').removeClass('navbar-fixed');
+    }
+  });
+
+  // automatically resize textarea to match article size
+  $(function() {
+      //  changes mouse cursor when highlighting loawer right of box
+      $(document).on('mousemove', 'textarea', function(e) {
+      var a = $(this).offset().top + $(this).outerHeight() - 16,	//	top border of bottom-right-corner-box area
+        b = $(this).offset().left + $(this).outerWidth() - 16;	//	left border of bottom-right-corner-box area
+      $(this).css({
+        cursor: e.pageY > a && e.pageX > b ? 'nw-resize' : ''
+      });
+    })
+      //  the following simple make the textbox "Auto-Expand" as it is typed in
+      .on('keyup', 'textarea', function(e) {
+          //  the following will help the text expand as typing takes place
+          while($(this).outerHeight() < this.scrollHeight + parseFloat($(this).css("borderTopWidth")) + parseFloat($(this).css("borderBottomWidth"))) {
+              $(this).height($(this).height()+1);
+          };
+      });
+  });
+});
