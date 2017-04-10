c22fef420e7332a40680fcaabb3685421c6f42f3
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2017-04-10 17:25:36 +0100

    :bug: Add title to safe parameters, replacing publish_date

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,6 +69,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:article, :author, :publish_date, :category)
+      params.require(:post).permit(:article, :author, :title, :category)
     end
 end
