856313c2fedbd6226e7adf6482f536152ab496f8
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2017-04-11 00:29:44 +0100

    :zap::wrench: Update routes to remove 'static_pages' from path
    About and Contact pages have tidier URLs

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/about'
+  get '/about' => 'static_pages#about'
 
-  get 'static_pages/contact'
+  get '/contact' => 'static_pages#contact'
 
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
