af6affa526f3a2d668cdc282e1bae4c799936979
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2017-05-18 18:00:01 +0100

    :wrench: Install Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Device mailer configuration for production
+  config.action_mailer.default_url_options = { host: 'stivaroscfblog.herokuapp.com', port: 3000 }
 end
