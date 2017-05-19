a0efec462e91f3c1dce889a6db9f9bc979f51d82
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2017-05-19 16:40:17 +0100

    :sparkles: Add validation to post and comment creation forms

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,8 +5,15 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = @post.comments.new(comment_params)
     @comment.user = current_user
-    @comment.save
-    redirect_to post_path(@post)
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @post, notice: 'Comment was successfully created.' }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { redirect_to @post, alert: @comment.errors.full_messages.to_sentence }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
   has_many :comments
+  validates :title, :article, :author, :category, presence: true
 end
